9b99a82cbde13af605ecb399d8798107911826c6
Adrian <120565330+Kriucz@users.noreply.github.com>
2023-03-25 16:49:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const allNavItems = document.querySelectorAll('.nav-link')
+	const navList = document.querySelector('.navbar-collapse')
+
+	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+})
+
 let buttons = document.querySelectorAll('#button')
 
 buttons.forEach(item => {
